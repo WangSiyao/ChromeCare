d4bee84d10ad67ba438b9dda9369f8ca8ad42915
Wang Siyao <1521925222@qq.com>
2018-10-04 00:04:42 -0400

    Sort Urls

--- arrange.js
+++ arrange.js
@@ -1,29 +1,44 @@
 
-var targetWindow = null;
-var tagArr = null;
+var windowsNum = null;
+var tabArr = null;
 
 var maxPerWindow = 15
 
 function start() {
-  tagArr = new Array();
+  tabArr = new Array();
   chrome.windows.getAll(getAllWindows);
 }
 
 function getAllWindows(wins) {
+  windowsNum = wins.length;
   for (i = 0; i < wins.length; i++) {
-    chrome.tabs.query({"windowId":wins[i].id}, getTagsInWindow)
+    chrome.tabs.query({"windowId":wins[i].id}, getTabsInWindow);
   }
 }
 
-function getTagsInWindow(tabs) {
+function getTabsInWindow(tabs) {
   for (i = 0; i < tabs.length; i++) {
-    tagArr.push(tabs[i]);
-    console.log(tabs[i].url);
+    tabArr.push(tabs[i]);
+  }
+  windowsNum--;
+  if (windowsNum == 0) {
+    sortAndRerrange();
   }
 }
 
 function sortAndRerrange() {
-  console.log(tagArr.length);
+  console.log(tabArr.length);
+  tabArr.sort(function(a, b) {
+    if (a.url < b.url) {
+      return -1;
+    }
+    else if (a.url > b.url) {
+      return 1;
+    }
+    return 0;
+  })
+  for (i = 0; i < tabArr.length; i++)
+    console.log(tabArr[i].url)
 }
 
 chrome.browserAction.onClicked.addListener(start);
